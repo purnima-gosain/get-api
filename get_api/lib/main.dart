81265caf2ff79dbb2cfd375ef483a41195825e2d
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-15 12:12:17 +0545

    feat:added provider in main page and made title in center

--- get_api/lib/main.dart
+++ get_api/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get_api/provider/provider.dart';
 import 'package:get_api/view/page/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.cyan,
       ),
-      home: const HomePage(),
+      home: ChangeNotifierProvider(
+          create: (_) => PostProvider(), child: const HomePage()),
     );
   }
 }
